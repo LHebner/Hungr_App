107893c15e5906d84d3ba845b14240d750a89b4e
Carson Neal <carsonlneal@gmail.com>
2021-12-09 16:10:04 -0500

    Working on search function in Vue with Yelp API

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -2,11 +2,15 @@
   <div class="home">
     <h1>Home</h1>
     <p>You must be authenticated to see this</p>
+    <p>Please enter your city or zip code</p>
   </div>
 </template>
 
 <script>
 export default {
-  name: "home"
+  name: "home",
+  components: {
+    Search
+  }
 };
 </script>
